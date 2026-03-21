06933eccadf98f3d42a5e9a4f863657788b29f59
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-21 13:00:11 +0300

    Made some updates on the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className='flex justify-between p-4 shadow-sm fixed w-full z-10 backdrop-blur-xl border border-gray-300 bg-white opacity-95  '>
       <div className='flex gap-2 items-center'>
         <IoCartOutline className='text-green-500' size={35}/>
-        <h3 className='text-3xl font-bold'>Grocery Shop</h3>
+        <h3 className='sm:text-3xl text-xl font-bold'>Grocery Shop</h3>
       </div>
 
     <button className='p-2 text-white font-semibold rounded-lg bg-black'>Get Started</button>
